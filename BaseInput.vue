aa682d252e7e8791b2ebc1d9f0e7d693b74b3250
sekopa <TSekopa@csir.co.za>
2022-03-04 15:49:10 +0200

    new vue

---
+++ BaseInput.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <label>{{ label}}</label><br>
+    <input 
+        :placeholder="label"
+        class="field "
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    >
+</div> 
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: string,
+            default: ''
+        },
+        modelValue: {
+            type: [String, Number],
+            default: ''
+        }
+    }
+}
+</script>
