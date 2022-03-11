848e0c1bfe4b69510ab29d8006c05cacb4d2cc80
GivenM2021 <pmalatjie@csir.co.za>
2022-03-11 12:53:20 +0200

    Revert "Added latest files"
    This reverts commit 6046775b7d99bc43c965c2f3f71cca884d25d264.

--- NOW/script.js
+++
@@ -1,9 +0,0 @@
-function preview_image(imgInput)
-{
-    var reader = new FileReader();
-    reader.onload = function(){
-        displayImage = document.getElementById('display_image')
-        displayImage.src = reader.result;
-    }
-    reader.readAsDataURL(imgInput.target.files[0])
-}
